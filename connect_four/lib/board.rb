e57117a2209a6b2cc33a6d8301fe15877e1a035e
Anthony Fuentes <anthony.c.fuentes@gmail.com>
2016-11-03 14:47:15 -0400

    Write tests for board

--- connect_four/lib/board.rb
+++ connect_four/lib/board.rb
@@ -3,18 +3,17 @@ public
   :board
   :add_piece
   :winner?
-  :win
-  :check_diagonal
-  :four_in_a_row
-  :check_horizontal
   :rows
-  :check_vertical
-  :negative_diagonal
-  :positive_diagonal
-  :scan_board
   :full?
 private
-  nil
+  :win
+  :check_diagonal
+  :negative_diagonal
+  :positive_diagonal
+  :check_horizontal
+  :check_vertical
+  :four_in_a_row
+  :scan_board
 =end
 
 require 'matrix'
@@ -26,6 +25,8 @@ class Board
   end
 
   def add_piece(column, piece)
+    # added during testing to protect against out of bounds
+    return false if (column > board.length - 1) || column < 0
     board[column].each_with_index do |cell, row|
       if !cell
         board[column][row] = piece
@@ -39,72 +40,6 @@ class Board
     !!win(coords)
   end
 
-  def win(coords)
-    # check_horizontal || check_vertical ||
-    check_diagonal(coords)
-  end
-
-  def four_in_a_row(section)
-    counter = 0
-    section.each.with_index(1) do |item, index|
-      if (item == section[index-1] && !item.nil?)
-        counter += 1
-      else
-        counter = 0
-      end
-      return true if counter == 4
-    end
-    false
-  end
-
-  def check_horizontal
-    rows.each do |row|
-      return true if four_in_a_row(row)
-    end
-    false
-  end
-
-  def check_vertical
-    board.each do |column|
-      return true if four_in_a_row(column)
-    end
-    false
-  end
-
-  def negative_diagonal(piece)
-    until piece[0] == 0 || piece[1] == 5
-      piece[0] -= 1
-      piece[1] += 1
-    end
-    (0..5).collect { |i| board[piece[0] + i][piece[1] - i] }
-  end
-
-  def positive_diagonal(piece)
-    until piece[0] == 0 || piece[1] == 0
-      piece[0] -= 1
-      piece[1] -= 1
-    end
-    (0..5).collect { |i| board[piece[0] + i][piece[1] + i] }
-  end
-
-  def check_diagonal(piece)
-    diagonals = []
-    diagonals << positive_diagonal(piece)
-    diagonals << negative_diagonal(piece)
-    diagonals.each do |diagonal|
-      return true if four_in_a_row(diagonal)
-    end
-    false
-  end
-
-  def scan_board
-    board.each do |column|
-      column.each do |cell|
-        yield cell
-      end
-    end
-  end
-
   def full?
     scan_board do |item|
       return false if !item
@@ -115,4 +50,82 @@ class Board
   def rows
     board.transpose.reverse
   end
+
+  # added for testing
+  def columns
+    board
+  end
+
+  # added for testing
+  def column(column_number)
+    board[column_number]
+  end
+
+  private
+
+    def check_diagonal(piece)
+      diagonals = []
+      diagonals << positive_diagonal(piece)
+      diagonals << negative_diagonal(piece)
+      diagonals.each do |diagonal|
+        return true if four_in_a_row(diagonal)
+      end
+      false
+    end
+
+    def check_horizontal
+      rows.each do |row|
+        return true if four_in_a_row(row)
+      end
+      false
+    end
+
+    def check_vertical
+      board.each do |column|
+        return true if four_in_a_row(column)
+      end
+      false
+    end
+
+    def negative_diagonal(piece)
+      until piece[0] == 0 || piece[1] == 5
+        piece[0] -= 1
+        piece[1] += 1 end
+      (0..5).collect { |i| board[piece[0] + i][piece[1] - i] }
+    end
+
+    def positive_diagonal(piece)
+      until piece[0] == 0 || piece[1] == 0
+        piece[0] -= 1
+        piece[1] -= 1
+      end
+      (0..5).collect { |i| board[piece[0] + i][piece[1] + i] }
+    end
+
+    def four_in_a_row(section)
+      counter = 0
+      section.each.with_index(1) do |item, index|
+        if (item == section[index-1] && !item.nil?)
+          counter += 1
+        else
+          counter = 0
+        end
+        return true if counter == 4
+      end
+      false
+    end
+
+    def scan_board
+      board.each do |column|
+        column.each do |cell|
+          yield cell
+        end
+      end
+    end
+
+    def win(coords)
+      # check_horizontal || check_vertical ||
+      check_diagonal(coords)
+    end
+
 end
--- connect_four/spec/board_spec.rb
+++ connect_four/spec/board_spec.rb
@@ -1,16 +1,70 @@
 require 'board'
 
 describe Board do
-
   let(:board) { Board.new }
 
   context 'a new board' do
-
     describe '#board' do
-
       it 'returns an empty board' do
         expect(board).to_not be_nil
       end
     end
   end
+
+  describe '#add_piece' do
+
+    it 'adds one piece to the given column if the column is in bounds' do
+      # less coupled w/ array
+      #column_number = 3
+      #board.add_piece(column_number, "T")
+      #piece_count_column_3 = board.column(column_number).count { |e| !e.nil? }
+      #expect(piece_count_column_3).to eq(1)
+      # coupled w/ array
+      column_number = 0
+      board.add_piece(column_number, "T")
+      expect(board.board[0][0]).to eq("T")
+    end
+
+    it 'returns false if column is out of bounds' do
+      column_number = board.board.length
+      expect(board.add_piece(column_number, "X")).to eq(false)
+    end
+
+    it 'returns false if column is full' do
+      column_number = 0
+      column = board.board[0]
+      column.length.times do
+        board.add_piece(column_number, "T")
+      end
+      expect(board.add_piece(column_number, "X")).to eq(false)
+    end
+
+  end
+
+  describe '#winner?' do
+
+    it 'returns false if there are not 4 identical pieces connected' do
+      board.add_piece(0, "X")
+      2.times { board.add_piece(1, "X") }
+      3.times { board.add_piece(2, "X") }
+    end
+
+    it 'returns true if there are 4 identical pieces connected'
+
+  end
+
+  describe '#rows' do
+
+    it 'returns all the boards rows'
+
+  end
+
+  describe '#full?' do
+
+    it 'returns false if the board is not full'
+
+    it 'returns true if the board is full'
+
+  end
+
 end
